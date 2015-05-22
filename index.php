e30ca4b79624271105d0b67298eef253e67913c9
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-05-22 11:18:45 -0700

    added bootstrap

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
 <?php 
  ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ 	<link rel="stylesheet" type="text/css" href="css/main.css"> 
+ 	<title></title>
+ </head>
+ <body>
+ 	<div class="navbuttons">
+	 	<a href="http://localhost/landingpage/aboutme.php"><button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+	  		ABOUT ME
+		</button></a>
+	
+	 	<a href="http://localhost/landingpage/contact.php"><button type="button" class="btn btn-contact" data-toggle="button" aria-pressed="false" autocomplete="off">
+	  		CONTACT
+		</button></a>
+
+		<a href="http://localhost/landingpage/projects.php"><button type="button" class="btn btn-projects" data-toggle="button" aria-pressed="false" autocomplete="off">
+	  		PROJECTS
+		</button></a>
+	</div>
+ 	<div class="test">
+   		<h1>JAH'MELAH MAAT-HOTEP</h1> 
+	</div>
+
+ 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+ 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+ </body>
+ </html>
